8164b0e1f600f34224f558cad06f5c98475fafc9
artytrik <tema-luch@mail.ru>
2019-08-24 09:29:23 +0300

    Исправил ошибки линтера

--- src/components/info.js
+++ src/components/info.js
@@ -1,7 +1,7 @@
 export const makeInfo = ({cities}) =>
- `<div class="trip-info__main">
+  `<div class="trip-info__main">
     <h1 class="trip-info__title">${cities.length > 3
-      ? `${cities[0]}&mdash; ... &mdash;${cities[cities.length-1]}`
-      : `${cities.join(` &mdash; `)}`}</h1>
+    ? `${cities[0]}&mdash; ... &mdash;${cities[cities.length - 1]}`
+    : `${cities.join(` &mdash; `)}`}</h1>
     <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
   </div>`;
