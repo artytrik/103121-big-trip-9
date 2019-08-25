1a596cbde4a0caff8236335706d5a30e328ca15d
artytrik <tema-luch@mail.ru>
2019-08-25 11:59:19 +0300

    Исправил ошибки линтера

--- src/components/filters.js
+++ src/components/filters.js
@@ -17,12 +17,12 @@ export class Filters {
   getTemplate() {
     return `<form class="trip-filters" action="#" method="get">
       ${this._filters.map(({name}) =>
-      `<div class="trip-filters__filter">
-        <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}">
-        <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
-      </div>`).join(``)}
+    `<div class="trip-filters__filter">
+      <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}">
+      <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
+    </div>`).join(``)}
 
-      <button class="visually-hidden" type="submit">Accept filter</button>
+    <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`;
   }
 }
--- src/components/info.js
+++ src/components/info.js
@@ -16,10 +16,10 @@ export class Info {
 
   getTemplate() {
     return `<div class="trip-info__main">
-      <h1 class="trip-info__title">${this._cities.length > 3
-      ? `${this._cities[0]}&mdash; ... &mdash;${this._cities[this._cities.length - 1]}`
-      : `${this._cities.join(` &mdash; `)}`}</h1>
-      <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
+    <h1 class="trip-info__title">${this._cities.length > 3
+    ? `${this._cities[0]}&mdash; ... &mdash;${this._cities[this._cities.length - 1]}`
+    : `${this._cities.join(` &mdash; `)}`}</h1>
+    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
     </div>`;
   }
 }
