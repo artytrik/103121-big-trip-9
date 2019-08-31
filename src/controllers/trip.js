d951823cd4d8a6e834564e52f145e1917fc7b55f
artytrik <tema-luch@mail.ru>
2019-08-31 10:23:01 +0300

    Реализовал сортировку

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -6,6 +6,7 @@ import {EmptyResult} from "../components/empty-result";
 import {render} from '../utils.js';
 import {Position} from '../utils.js';
 import {Day} from '../components/day.js';
+import {Sort} from '../components/sort.js';
 
 export class TripController {
   constructor(container, points) {
@@ -15,18 +16,23 @@ export class TripController {
     this._day = new Day();
     this._tripEvents = new TripEvents();
     this._emptyResult = new EmptyResult();
+    this._sort = new Sort();
   }
 
   init() {
-    render(this._container, this._tripDays.getElement(), Position.AFTEREND);
-    render(this._tripDays.getElement(), this._day.getElement(), Position.AFTERBEGIN);
-    render(this._day.getElement(), this._tripEvents.getElement(), Position.AFTERBEGIN);
+    render(this._tripDays.getElement(), this._day.getElement(), Position.BEFOREEND);
+    render(this._day.getElement(), this._tripEvents.getElement(), Position.BEFOREEND);
+    render(this._container, this._sort.getElement(), Position.BEFOREEND);
+    render(this._container, this._tripDays.getElement(), Position.BEFOREEND);
 
     if (this._points.length > 0) {
       this._points.forEach((point) => this._renderPoints(point));
     } else {
       render(this._tripEvents.getElement(), this._emptyResult.getElement(), Position.BEFOREEND);
     }
+
+    this._sort.getElement()
+      .addEventListener('change', (evt) => this._onSortLinkClick(evt));
   }
 
   _renderPoints(point) {
@@ -55,5 +61,25 @@ export class TripController {
       });
 
     render(this._tripEvents.getElement(), tripPoint.getElement(), Position.BEFOREEND);
+  }
+
+  _onSortLinkClick(evt) {
+    evt.preventDefault();
+
+    this._tripEvents.getElement().innerHTML = ``;
+
+    switch (evt.target.dataset.sortType) {
+      case `event`:
+        this._points.forEach((point) => this._renderPoints(point));
+        break;
+      case `time`:
+        const sortedByTime = this._points.slice().sort((a, b) => a.dueDate - b.dueDate);
+        sortedByTime.forEach((point) => this._renderPoints(point));
+        break;
+      case `price`:
+        const sortedByPrice = this._points.slice().sort((a, b) => a.price - b.price);
+        sortedByPrice.forEach((point) => this._renderPoints(point));
+        break;
+    }
   }
 }
